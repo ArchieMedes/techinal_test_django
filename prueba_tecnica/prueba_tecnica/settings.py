c670ae967c6075c165f318befd523d341dc74828
ArchieMedes <archie.medes.97@gmail.com>
2023-02-07 00:20:55 -0600

    removed secret key to env file and added this to gitignore file

--- prueba_tecnica/prueba_tecnica/settings.py
+++ prueba_tecnica/prueba_tecnica/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#)e9!7iojgmvzfa^k9*t0)5f(ytix))k_v3(_q@yr83qow59ya'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
